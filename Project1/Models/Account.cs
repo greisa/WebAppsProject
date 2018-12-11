0b22fe291dcc9fbb2a63c1404d75b8d300764039
greisa <43643804+greisa@users.noreply.github.com>
2018-12-11 14:00:26 -0500

    Add files via upload

--- Project1/Models/Account.cs
+++ Project1/Models/Account.cs
@@ -1,14 +1,14 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Project1.Models
 {
-  public class Account
-  {
+    public class Account
+    {
         public int AccountID { get; set; }
-        
+
         public string UserName { get; set; }
 
         public string Password { get; set; }
--- Project1/Models/User.cs
+++ Project1/Models/User.cs
@@ -1,11 +1,11 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Project1.Models
 {
-  public class User : IdentityUser
-  {
-  }
+    public class User : IdentityUser
+    {
+    }
 }
